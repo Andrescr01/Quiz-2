dab1c2a391a0644cc573fcb6612737c6d2c301e1
Leonardo Briseño <hectorspacejam@gmail.com>
2018-10-15 08:32:57 -0500

    bocina

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BocinaPage } from '../bocina/bocina';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+bocina = BocinaPage; 
   constructor(public navCtrl: NavController) {
 
   }
-
+Clickbocina() {
+  this.navCtrl.push(this.bocina);
+}
 }
